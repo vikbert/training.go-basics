21b70d3e74294cd0139446e90a37a1873f6c143a
Thomas Auinger <thomas@auinger.de>
2023-02-13 10:33:26 +0100

    changed types away from BinaryXy

--- 025_methods-interfaces/c_interfaces/main.go
+++ 025_methods-interfaces/c_interfaces/main.go
@@ -6,7 +6,7 @@ func main() {
 	fmt.Println("Interfaces:")
 }
 
-//func check(n int, f BinaryFormatter, p BinaryParser) {
+//func check(n int, f Formatter, p Parser) {
 //	out := p.parse(f.format(n))
 //	if out != n {
 //		panic(fmt.Sprintf("got %d, wanted %d", out, n))
