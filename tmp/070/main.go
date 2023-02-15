a5dbe2b8aecc579e0348079bc78515538cc5e556
Thomas Auinger <thomas@auinger.de>
2023-02-15 13:18:33 +0100

    070 tmp

--- tmp/070/main.go
+++ tmp/070/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func simpleChannel() {
@@ -10,11 +11,52 @@ func simpleChannel() {
 	ch <- 1
 }
 
+func bufferedChannel() {
+	ch := make(chan int, 100)
+	// we can write into channel although there
+	// is no consuming goroutine
+	ch <- 1
+	ch <- 2
+	ch <- 3
+}
+
+func closeChannel() {
+	dummyChannel := make(chan int)
+	go func(ch chan int) {
+		fmt.Println("Waiting for input...")
+		n, exists := <-ch
+		fmt.Println(n, exists)
+	}(dummyChannel)
+	time.Sleep(time.Second)
+	close(dummyChannel)
+	time.Sleep(time.Second)
+}
+
+func readUntilClosed(ch chan int) {
+	for {
+		input, exists := <-ch
+		if !exists {
+			fmt.Println("Feierabend!")
+			return
+		} else {
+			fmt.Println(input) // work with input
+		}
+	}
+}
+
+func readUntilClosedRanged(ch chan int) {
+	for input := range ch {
+		fmt.Println(input) // work with input
+	}
+}
+
 func main() {
-	ch := make(chan string)
-	go sayHello(ch)
-	ch <- "Tom"
-	ch <- "Jules"
+	ch := make(chan int)
+	go readUntilClosedRanged(ch)
+	ch <- 1
+	ch <- 2
+	close(ch)
+	time.Sleep(time.Second)
 }
 
 func sayHello(ch chan string) {
