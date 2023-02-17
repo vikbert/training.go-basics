3d44ce647325c8224ae0c46baa2edda1fbffa110
Thomas Auinger <thomas@auinger.de>
2023-02-17 13:22:42 +0100

    pipeline code

---
+++ tmp/070/proc-pipeline/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	doneCh := make(chan struct{})
+	result := collector(
+		mapper(
+			filter(gen(doneCh), func(n int) bool { return n%2 == 0 }),
+		),
+	)
+	time.Sleep(time.Second)
+	doneCh <- struct{}{}
+	fmt.Println(*result)
+}
+
+func gen(done chan struct{}) chan int {
+	ch := make(chan int)
+	go func() {
+		fmt.Println("Generator stage starting")
+		i := 0
+		for {
+			select {
+			case ch <- i:
+				time.Sleep(100 * time.Millisecond)
+				i++
+			case <-done:
+				fmt.Println("Generator stage stopping")
+				close(ch)
+				return
+			}
+		}
+	}()
+	return ch
+}
+
+func filter(inCh chan int, filterFunc func(int) bool) chan int {
+	outCh := make(chan int)
+	go func() {
+		fmt.Println("Filter stage starting")
+		for n := range inCh {
+			if filterFunc(n) {
+				outCh <- n
+			}
+		}
+		fmt.Println("Filter stage stopping")
+		close(outCh)
+	}()
+	return outCh
+}
+
+func mapper(inCh chan int) chan int {
+	outCh := make(chan int)
+	go func() {
+		fmt.Println("Mapper stage starting")
+		for n := range inCh {
+			outCh <- n * n
+		}
+		fmt.Println("Mapper stage stopping")
+		close(outCh)
+	}()
+	return outCh
+}
+
+func collector(inCh chan int) *[]int {
+	res := make([]int, 0, 10)
+	go func() {
+		fmt.Println("Collector stage starting")
+		for n := range inCh {
+			res = append(res, n)
+		}
+		fmt.Println("Collector stage stopping")
+	}()
+	return &res
+}
