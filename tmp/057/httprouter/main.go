d574e0af0591a90bc608fade46614f14eb441c37
Thomas Auinger <thomas@auinger.de>
2023-06-21 12:27:28 +0200

    finished tmp/057-httprouter

--- tmp/057/httprouter/main.go
+++ tmp/057/httprouter/main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 )
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "Hello, %s!\n", ps.ByName("name"))
+func HelloHandler(wrt http.ResponseWriter, req *http.Request, prms httprouter.Params) {
+	name := prms.ByName("name")
+	if name == "" {
+		fmt.Fprintf(wrt, "Hello!\n")
+	} else {
+		fmt.Fprintf(wrt, "Hello, %s!\n", name)
+	}
+}
+
+func CreateItemHandler(wrt http.ResponseWriter, req *http.Request, prms httprouter.Params) {
+	var reqData struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	}
+	if json.NewDecoder(req.Body).Decode(&reqData) == nil {
+		fmt.Printf("Got create-item request: %+v\n", reqData)
+		wrt.WriteHeader(http.StatusCreated)
+	}
 }
 
 func main() {
-	var x = "hall"
 	router := httprouter.New()
-	router.GET("/hello/:name", Hello)
-
+	router.GET("/hello", HelloHandler)
+	router.GET("/hello/:name", HelloHandler)
+	router.POST("/items", CreateItemHandler)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
