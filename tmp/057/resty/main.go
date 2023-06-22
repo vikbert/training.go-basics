ed467d2c6f511ec6b5171f924b684a5187b0f142
Thomas Auinger <thomas@auinger.de>
2023-06-22 11:17:12 +0200

    added 057-resty

---
+++ tmp/057/resty/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-resty/resty/v2"
+)
+
+func main() {
+	client := resty.New()
+	client.SetHeader("Content-Type", "application/json")
+	client.SetHeader("Accept", "application/json")
+
+	type GetPostsResult struct {
+		// ...
+	}
+	rsp, err := client.R().
+		SetQueryString("details=true&page=1").
+		SetAuthToken("BC594900518B4F7EAC75BD37F019E08FBC594900518B4F7EAC75BD37F019E08F").
+		SetResult(GetPostsResult{}).
+		Get("https://jsonplaceholder.typicode.com/posts")
+	fmt.Println(rsp.Status())
+	fmt.Println(string(rsp.Body()[0:100]))
+
+	rsp, err = client.R().
+		SetBody(`{"productId":"S-123", "name":"Pizza Funghi"}`).
+		Post("https://myapp.com/login")
+}
