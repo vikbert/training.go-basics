785bc630c0cbd10c5d242b98a007285d65f7b663
Thomas Auinger <thomas@auinger.de>
2023-06-27 13:35:27 +0200

    gorm

---
+++ tmp/082/gorm/go.mod
@@ -0,0 +1,11 @@
+module local/tmp/082_gorm
+
+go 1.20
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	gorm.io/driver/sqlite v1.5.2 // indirect
+	gorm.io/gorm v1.25.2-0.20230530020048-26663ab9bf55 // indirect
+)
---
+++ tmp/082/gorm/gorm.go
@@ -0,0 +1,27 @@
+package gorm
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	// Read
+	var product Product
+	db.First(&product, 1)
+	db.First(&product, "code = ?", "D42")
+}
