00e5618a68d4126b3c8e5f6cfd4567f022375a95
vikbert <segentor@gmail.com>
2023-07-03 15:07:01 +0200

    c_types

--- 020-syntax/c_types/main.go
+++ 020-syntax/c_types/main.go
@@ -2,12 +2,29 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Types:")
-	// var lengthMeter = centimeterToMeter(lengthCentimeter)
+type Centimeter float32
+type Meter float32
 
-	// Ausgabe: "140 cm equals 1.40 m"
+func (m *Meter) toCentimeter() Centimeter {
+	return Centimeter(float32(*m) * 100)
 }
 
-func centimeterToMeter() {
+func (cm *Centimeter) toMeter() Meter {
+	return Meter(float32(*cm) / 100)
+}
+
+func main() {
+	fmt.Println("Types:")
+
+	lengthCentimeter := Centimeter(140.0)
+	var lengthMeter = centimeterToMeter(lengthCentimeter)
+
+	fmt.Printf("Type of lengthCentimeter: %T\n", lengthCentimeter)
+
+	// Ausgabe: "140 cm equals 1.40 m"
+	fmt.Printf("%.2f cm equals %.2f m", lengthCentimeter, lengthMeter)
+}
+
+func centimeterToMeter(cm Centimeter) Meter {
+	return cm.toMeter()
 }
