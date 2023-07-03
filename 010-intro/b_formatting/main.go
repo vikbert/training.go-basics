ea8030c8711e1d7ff0b651537666671bb36765a2
vikbert <segentor@gmail.com>
2023-07-03 13:45:27 +0200

    updates

--- 010-intro/b_formatting/main.go
+++ 010-intro/b_formatting/main.go
@@ -5,11 +5,12 @@ import (
 )
 
 func main() {
-	var number = 40
+	var number int64 = 40
 
 	fmt.Println("Formatting:")
 	fmt.Println("Number: ", number)
 	fmt.Printf("Number Prefix: %04d\n", number)
+	fmt.Printf("Float points: %.4f\n", 3.1)
 	fmt.Println("Number Binary: ", fmt.Sprintf("%b", number))
 	fmt.Println("Number Hex: ", fmt.Sprintf("%X", number))
 	fmt.Println("Number Type: ", fmt.Sprintf("%T", number))
--- 020-syntax/a_variables-and-constants/main.go
+++ 020-syntax/a_variables-and-constants/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Variables & constants:")
+
+	var kb = 1 << 10
+	fmt.Println("KB", kb)
+
+	var mb = 1 << 20
+	fmt.Println("MB", mb)
+
+	var gb = 1 << 30
+	fmt.Println("GB", gb)
+
+	const (
+		_ = 1 << (10 * iota)
+		KB
+		MB
+		GB
+	)
+
+	fmt.Println("KB: ", KB)
+	fmt.Println("MB: ", MB)
+	fmt.Println("GB: ", GB)
 }
