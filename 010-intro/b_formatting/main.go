64e8c6ea0eeec77b4efd68bd1e49827301397c00
vikbert <segentor@gmail.com>
2023-07-03 11:02:47 +0200

    updates

--- 010-intro/b_formatting/main.go
+++ 010-intro/b_formatting/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
+	var number int64 = 40
 	fmt.Println("Formatting:")
+	fmt.Println("Number: ", number)
+	fmt.Println("Number Prefix: ", fmt.Sprintf("%04d", number))
+	fmt.Println("Number Binary: ", strconv.FormatInt(number, 2))
+	fmt.Println("Number Hex: ", fmt.Sprintf("%X", number))
+	fmt.Println("Number Type: ", fmt.Sprintf("%T", number))
 }
