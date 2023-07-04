a03a6bd23bd8cecf28241557d72538754374374a
vikbert <segentor@gmail.com>
2023-07-04 09:59:47 +0200

    updates

--- 020-syntax/o_slices/main.go
+++ 020-syntax/o_slices/main.go
@@ -2,6 +2,52 @@ package main
 
 import "fmt"
 
+type SliceStack struct {
+	Data []interface{}
+}
+
 func main() {
-	fmt.Println("Slices:")
+	myStack := SliceStack{}
+	myStack.Push(0)
+	myStack.Push(1)
+	myStack.Push(2)
+
+	fmt.Println("Slices:", myStack)
+	fmt.Println("Slices::Pop:", myStack.Pop())
+	fmt.Println("Slices:", myStack)
+	fmt.Println("Slices::Peek:", myStack.Peek())
+	fmt.Println("Slices:", myStack)
+	fmt.Println("Slices::Size:", myStack.Size())
+	fmt.Println("Slices::IsEmpty:", myStack.IsEmpty())
+}
+
+func (s *SliceStack) Push(value interface{}) {
+	s.Data = append(s.Data, value)
+}
+
+func (s *SliceStack) Pop() interface{} {
+	if s.IsEmpty() {
+		return nil
+	}
+
+	value := s.Data[0]
+	s.Data = s.Data[1:len(s.Data)]
+
+	return value
+}
+
+func (s *SliceStack) Peek() interface{} {
+	if s.IsEmpty() {
+		return nil
+	}
+
+	return s.Data[0]
+}
+
+func (s *SliceStack) Size() int {
+	return len(s.Data)
+}
+
+func (s *SliceStack) IsEmpty() bool {
+	return len(s.Data) == 0
 }
