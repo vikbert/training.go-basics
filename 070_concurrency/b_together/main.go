a334a53d2f54b2b2499339b0048f70c71937e5cd
vikbert <segentor@gmail.com>
2023-07-10 22:46:23 +0200

    updates

--- 070_concurrency/b_together/main.go
+++ 070_concurrency/b_together/main.go
@@ -8,46 +8,52 @@ import (
 )
 
 type Runner struct {
-	name string
+	name  string
+	score int
 }
 
 func (runner *Runner) run() {
 	s := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(s)
-	duration := r.Intn(10)
+	duration := r.Intn(1000)
 
-	fmt.Printf("%s: start running\n %ds\n", runner.name, duration)
-	time.Sleep(time.Duration(duration) * time.Second)
+	fmt.Println("start running: ", runner.name)
+	time.Sleep(time.Duration(duration) * time.Millisecond)
+	runner.score = duration
 }
 
-func startRace(runner Runner, awarding chan string, wg *sync.WaitGroup) {
+func startRace(runner Runner, register chan Runner, wg *sync.WaitGroup) {
 	runner.run()
 	wg.Done()
-	name := <-awarding
-	fmt.Printf("Awarding: %s %s\n", name, time.Now())
+
+	register <- runner // register the name to the channel, but it is blocked, if no name is read
+
+	fmt.Printf("\n%s Awarding time: %s", time.Now().Format(time.StampNano), runner.name)
 }
 
 func main() {
 	wg := sync.WaitGroup{}
 	runners := []Runner{
-		Runner{"Tom"},
-		{"Jerry"},
-		{"Donald"},
+		Runner{name: "Tom"},
+		{name: "Jerry"},
+		{name: "Donald"},
 	}
 
-	awarding := make(chan string)
+	register := make(chan Runner)
 
-	fmt.Println("Start running ...\n")
+	fmt.Println("\n\n100-Meter race ...\n")
 	for _, runner := range runners {
 		wg.Add(1)
-		go startRace(runner, awarding, &wg)
+		go startRace(runner, register, &wg)
 	}
 
 	wg.Wait()
-	for _, runner := range runners {
-		awarding <- runner.name
-	}
-	time.Sleep(time.Duration(10) * time.Second)
 
-	fmt.Println("\nAll runners completed the course!")
+	for index, _ := range runners {
+		// read the registered name and it frees the blocked process, see line-29
+		runner := <-register
+		fmt.Printf("\nPlatz %d (%ds) %s\n", index+1, runner.score, runner.name)
+	}
+
+	time.Sleep(time.Duration(5) * time.Second)
 }
