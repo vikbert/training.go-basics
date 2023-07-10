eb53335ee64afcd8506d280addb9a5d97dd06ce9
vikbert <segentor@gmail.com>
2023-07-10 12:46:14 +0200

    updates

--- 042_methods-interfaces/a_methods/main.go
+++ 042_methods-interfaces/a_methods/main.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"local/042_methods-interfaces/a_methods/stack"
+)
 
 type book struct {
 	isbn      string
 	author    string
 	publisher string
+}
+
+type Bookshelf struct {
+	books []book
 }
 
 func main() {
@@ -17,5 +25,49 @@ func main() {
 		{"423-001", "I. Wellmann", "Random House"},
 		{"193-753", "T. Müller", "Wort und Bild Verlag"},
 	}
-	fmt.Println(books)
+
+	bs := Bookshelf{books[:]}
+	bs.add(book{
+		isbn:      "999-999",
+		author:    "T. Hanks",
+		publisher: "Springer",
+	})
+
+	fmt.Println(bs)
+	fmt.Println(bs.forIsbn("999-999"))
+	fmt.Println(bs.all())
+
+	// stack demo
+	myStack := stack.NewStack()
+	myStack.Push(0)
+	myStack.Push(1)
+	myStack.Push(2)
+
+	fmt.Println("Stack:", myStack)
+	fmt.Println("Stack::Pop:", myStack.Pop())
+	fmt.Println("Stack::size:", myStack.Size())
+	fmt.Println("Stack:", myStack)
+	fmt.Println("Stack::Peek:", myStack.Peek())
+	fmt.Println("Stack::Size:", myStack.Size())
+	fmt.Println("Stack:", myStack)
+	fmt.Println("Stack::Size:", myStack.Size())
+}
+
+func (bs *Bookshelf) add(b book) {
+	bs.books = append(bs.books, b)
+}
+
+func (bs *Bookshelf) forIsbn(isbn string) (book, error) {
+	var found book
+	for _, b := range bs.books {
+		if b.isbn == isbn {
+			return b, nil
+		}
+	}
+
+	return found, errors.New("not found")
+}
+
+func (bs *Bookshelf) all() []book {
+	return bs.books
 }
--- 042_methods-interfaces/c_interfaces/main.go
+++ 042_methods-interfaces/c_interfaces/main.go
@@ -1,14 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+type Parser interface {
+	parse(string) int
+}
+
+type Formatter interface {
+	format(int) string
+}
+
+type BinaryFormatter struct{}
+type BinaryParser struct{}
+
+func (bf BinaryParser) parse(s string) int {
+	value, err := strconv.ParseInt(s, 2, 64)
+	if err != nil {
+		panic("Faided to convert string to binary")
+	}
+
+	fmt.Println("binary value", value)
+	return int(value)
+}
+
+func (bf BinaryFormatter) format(i int) string {
+	formatInt := strconv.FormatInt(int64(i), 2)
+	fmt.Println("int value", formatInt)
+	return formatInt
+}
 
 func main() {
 	fmt.Println("Interfaces:")
+	var formatter Formatter = BinaryFormatter{}
+	var parser Parser = BinaryParser{}
+
+	check(8, formatter, parser)
+
+	printMethods(parser)
+	fmt.Println("Done")
 }
 
-//func check(n int, f Formatter, p Parser) {
-//	out := p.parse(f.format(n))
-//	if out != n {
-//		panic(fmt.Sprintf("got %d, wanted %d", out, n))
-//	}
-//}
+func check(n int, f Formatter, p Parser) {
+	out := p.parse(f.format(n))
+	if out != n {
+		panic(fmt.Sprintf("got %d, wanted %d", out, n))
+	}
+}
+
+func printMethods(target any) {
+	t := reflect.TypeOf(&target)
+
+	for i := 0; i < t.NumMethod(); i++ {
+		m := t.Method(i)
+		fmt.Println(m.Name)
+	}
+}
