56ebd5584cbd6c892b3ba983774a2cff69f26389
vikbert <segentor@gmail.com>
2023-07-10 10:36:26 +0200

    updates

--- 040_testing/stack_test.go
+++ 040_testing/stack_test.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
+func setup() {
+	fmt.Printf("\033[1;36m%s\033[0m", "> Setup completed\n")
+}
+
+func teardown() {
+	fmt.Printf("\033[1;36m%s\033[0m", "> Teardown completed")
+	fmt.Printf("\n")
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	exitVal := m.Run()
+	teardown()
+	os.Exit(exitVal)
+}
+
 func TestSliceStack_IsEmpty(t *testing.T) {
 	s := SliceStack{}
-	assert.Empty(t, s, "Init stack should be empty")
+	assert.True(t, s.IsEmpty(), "Init stack should be empty")
 }
 
 func TestSliceStack_Push(t *testing.T) {
@@ -29,5 +47,5 @@ func TestSliceStack_Pop(t *testing.T) {
 	s.Push(12)
 	got := s.Pop()
 	assert.Equal(t, 12, got, "Stack should return the expected value")
-	assert.Empty(t, s, "Stack should be empty after remove the single element")
+	assert.True(t, s.IsEmpty(), "Stack should be empty after remove the single element")
 }
